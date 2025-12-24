152e3377b2d0f1fd31c985d952adafaf4ad97fd3
ray0404 <ray.valentin.04@gmail.com>
2025-12-24 02:44:42 -0700

    feat: add IDX development environment configuration

---
+++ .idx/dev.nix
@@ -0,0 +1,53 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+    pkgs.nodePackages.firebase-tools
+  ];
+  # Sets environment variables in the workspace
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "dsznajder.es7-react-js-snippets"
+      "bradlc.vscode-tailwindcss"
+      "esbenp.prettier-vscode"
+      "dbaeumer.vscode-eslint"
+    ];
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a continuous build in the background
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+    # Preview configuration
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          # Example: run "npm run dev" with PORT set to IDX's defined port for previews
+          command = [ "npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0" ];
+          manager = "web";
+          env = {
+            # Environment variables for your command
+            PORT = "$PORT";
+          };
+        };
+        firebase = {
+          command = [ "firebase" "emulators:start" "--only" "hosting" ];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
